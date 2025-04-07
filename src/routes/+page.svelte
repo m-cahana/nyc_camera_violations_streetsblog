5da29f0c708712064ef282624eb2ce4bfa4986fe
Michael Cahana <cahanamichael@gmail.com>
2025-04-07 18:26:00 -0400

    text updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,73 +10,45 @@
 
 <main>
 	<h1>Drivers Speed Through New York City's School Zones With No Limits</h1>
-    <div class="subtitle">March 2025</div>
+    <div class="subtitle">April 2025</div>
 
     <p>
         Last fiscal year, around 3 million drivers sped through school zones
-        in New York City, racking up nearly 8 million violations. Below you can see a representative sample of drivers with violations. Each driver is depicted as a circle, whose size is based on driver violations; the larger the circle, the more violations the driver committed. 
+        in New York City, racking up nearly 8 million violations. Below you can see a representative sample of drivers with violations. Each driver is depicted as a circle, whose size is based on the driver's violations; the larger the circle, the more violations the driver committed. 
     </p>
 
 	<CirclePack />
 
     <p>
-        [More graphics to be inserted below...]
+        Extreme offenders in New York City can rack up 755 violations because, effectively, they can speed through school zones as much as they please. They're hit with a $50 fine per speeding violation, but as long as they pay their fines, they won't be taken off the road. 
     </p>
 
 
     <p>
-        This small group of top offenders drove mostly around Brooklyn and
-        Queens, New York's two most populous boroughs. These boroughs also
-        have light traffic, relative to Manhattan at least, which may allow
-        for easier speeding.
-    </p>
-    
-    <p>
-        As evidenced by one driver racking up over 750 violations, top
-        offenders in New York can speed through school zones as much as they
-        please. They pay a $50 fine per violation, but their cars aren't
-        taken off the road, no matter how often they're fined. A city
-        abatement program used to require some drivers with 15 or more
-        violations to take a two-hour safety course, but it expired in
+        There used to be a city program in place to change behavior among extreme offenders. Dubbed the Dangerous Vehicle Abatement Program, in theory it was meant to target drivers with 15+ violations, mandating them to take a two-hour safety course. However, expired in
         <a
         href="https://www.nyc.gov/html/dot/downloads/pdf/dvap-faq.pdf"
         target="_blank"
         rel="noopener noreferrer"
-        >2023.</a>
+        >2023.</a>. And even when it was active, it only required 1,600 drivers to take the
+        safety course, out of the tens of thousands who were eligible. Beyond mandating a
+        safety course, the program rarely took further action; only 12 extreme offenders had their cars seized during the few years the abatement program was <a
+        href="https://www.nyc.gov/html/dot/downloads/pdf/evaluation-of-dvap.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+        >active</a>; all other offenders could continue driving without truly being restrained. 
     </p>
+    
 
     <p>
-        Even when it was active, it only required 1,600 drivers to take the
-        course, out of the tens of thousands eligible. Beyond mandating a
-        safety course, the city rarely took other action, only seizing cars
-        from 12 top offenders during the few years the abatement program was
-        <a
-          href="https://www.nyc.gov/html/dot/downloads/pdf/evaluation-of-dvap.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-          >active.</a
-        >
-    </p>
-
-    <p>
-        While the city doesn't impose serious consequences on top offenders,
-            their driving has deadly consequences for pedestrians. In 2021, a
-            driver with over 90 violations sped the wrong way up Gates Avenue in
-            Fort Greene, killing a 3-month old
-            <a
-              href="https://nyc.streetsblog.org/2021/09/12/slaughter-of-the-innocents-city-in-outrage-over-brooklyn-babys-death-by-reckless-driver"
-              target="_blank"
-              rel="noopener noreferrer"
-              >baby.</a
-            >
-            In 2024, another driver with nearly 30 violations struck and killed
-            a pedestrian in
-            <a
-              href="https://nyc.streetsblog.org/2024/01/17/a-pedestrian-was-killed-in-dyker-heights-by-a-driver-with-a-long-record-of-recklessness"
-              target="_blank"
-              rel="noopener noreferrer"
-              >Dyker Heights.</a
-            >
+        While the city doesn't impose serious consequences on top offenders, their driving has deadly consequences for pedestrians. In 2021, a driver with over 90 violations sped the wrong way up Gates Avenue in Fort Greene, killing a 3-month old <a
+        href="https://nyc.streetsblog.org/2021/09/12/slaughter-of-the-innocents-city-in-outrage-over-brooklyn-babys-death-by-reckless-driver"
+        target="_blank"
+        rel="noopener noreferrer"
+        >baby.</a> In 2024, another driver with nearly 30 violations struck and killed
+        a pedestrian in <a href="https://nyc.streetsblog.org/2024/01/17/a-pedestrian-was-killed-in-dyker-heights-by-a-driver-with-a-long-record-of-recklessness"
+        target="_blank"
+        rel="noopener noreferrer">Dyker Heights.</a>
     </p>
 
     <p>
